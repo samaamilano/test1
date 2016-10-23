8069c9683d764bcf4454da94d61965dc500a88c1
samaamilano <samaa.milano@hotmail.com>
2016-10-23 14:29:25 +0200

    frist edited

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,6 +17,7 @@ class Welcome extends CI_Controller {
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 ----
 	 */
 	public function index()
 	{
